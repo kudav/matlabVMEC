08c94bc547a4a49c3424d9a9997eb20175ec51d5
David Kulla <david.kulla@ipp.mpg.de>
2024-07-31 13:41:07 +0200

    Fixed srhoxi normalization+added flag

--- BEAMS3D/beams3d_calc_srhoxi.m
+++ BEAMS3D/beams3d_calc_srhoxi.m
@@ -11,6 +11,8 @@ function [srhoxi,beam_data] = beams3d_calc_srhoxi(beam_data,ind,npitch,varargin)
 %
 %   Options:
 %       'plot':        Plots the 2D birth map after calculation
+%       'normalize':   Normalizes srhoxi to the radial deposition profile
+%                       (recommended if numel(ind)>1)
 %
 %   Usage:
 %   beam_data = read_beams3d('beams3d_test.h5');
@@ -20,64 +22,85 @@ function [srhoxi,beam_data] = beams3d_calc_srhoxi(beam_data,ind,npitch,varargin)
 %   Version:        1.0
 %   Date:           1/24/24
 lplot=0;
-if strcmp(varargin,'plot')
-    lplot=1;
+lnormalize=0;
+if ~isempty(varargin)
+    i = 1;
+    while i <= numel(varargin)
+        switch varargin{i}
+            case 'plot'
+                lplot=1;
+            case 'normalize'
+                lnormalize=1;
+        end
+        i=i+1;
+    end
 end
 if ~isfield(beam_data,'vperp')
-beam_data.vperp=beams3d_calc_vperp(beam_data);
+    beam_data.vperp=beams3d_calc_vperp(beam_data);
 end
 if ~isfield(beam_data,'pitch')
-beam_data.pitch=beam_data.vll_lines./sqrt(beam_data.vll_lines.^2+beam_data.vperp.^2);
+    beam_data.pitch=beam_data.vll_lines./sqrt(beam_data.vll_lines.^2+beam_data.vperp.^2);
 end
 if ~isfield(beam_data,'birth')
-beam_data.birth = beams3d_calc_depo(beam_data,'depo_only');
+    beam_data.birth = beams3d_calc_depo(beam_data,'depo_only');
 end
 
 [s, ~, dVds] = beams3d_volume(beam_data);
-x=0:1.0/beam_data.ns_prof1:1;
 %c=(x(1:end-1)+x(2:end)).*0.5;
 %Match beams3d_profiles here
 nc = 128;
-dc = 1./(nc);
+dc = 1/(nc);
 rhoedges = 0:dc:1;
+%rhoedges=[0 beam_data.rho(1:end-1)+diff(beam_data.rho)/2, 1, 1.5]; %correct rho edges
+pitchedges=linspace(-1.,1.,npitch+1);
 c=(rhoedges(1:end-1)+rhoedges(2:end)).*0.5;
+p=(pitchedges(1:end-1)+pitchedges(2:end)).*0.5;
 
 dex=beam_data.end_state==0|beam_data.end_state==1; %Only particles that arent promptly lost
 f=sqrt(beam_data.S_lines(ind,dex));
 
-%rhoedges=[0 beam_data.rho(1:end-1)+diff(beam_data.rho)/2, 1, 1.5]; %correct rho edges
-pitchedges=linspace(-1.,1.,npitch);
+
 
 [discrho, ~]=discretize(f,rhoedges);
 [discpitch, ~]=discretize(beam_data.pitch(ind,dex),pitchedges);
-discpitch(isnan(discpitch))=numel(pitchedges)+1;
-discrho(isnan(discrho))=numel(rhoedges);
+discpitch(isnan(discpitch))=npitch+1;
+discrho(isnan(discrho))=nc+1;
 
 tmp=repmat(beam_data.Weight(dex)./numel(ind),1,numel(ind))'; %repeat and normalize
 beam=repmat(beam_data.Beam(dex),1,numel(ind))';
 
-srhoxi=accumarray([discrho(:),discpitch(:),beam(:)],tmp(:),[numel(rhoedges), numel(pitchedges)+1, max(beam_data.Beam)]);
+srhoxi=accumarray([discrho(:),discpitch(:),beam(:)],tmp(:),[nc+1, npitch+1, max(beam_data.Beam)]);
 srhoxi=srhoxi(1:end-1,1:end-1,:); %Truncate ends from nan and normalize to density
 
 v=pchip([0 sqrt(s)],[0 dVds.*2.*sqrt(s)],c);
-srhoxi=numel(pitchedges)/2*numel(rhoedges)*srhoxi./v'; %Volume normalization
-tmp1 = trapz(pitchedges,squeeze(sum(srhoxi,3)),2);
-tmp2=sum(beam_data.birth,1);
-tmp2=interp1(beam_data.dist_rhoaxis,tmp2,c,'linear','extrap');
-tmp3=tmp2'./tmp1;
-srhoxi=srhoxi./tmp3;
-tmp4 = trapz(pitchedges,squeeze(sum(srhoxi,3)),2);
+%As we can specify more orbit indices which would alter the total
+%deposition flux, we renormalize to the correct one from beams3d to only
+%alter the pitch/radius distribution, not the total flux.
+srhoxi=nc/2*npitch*srhoxi./v'; %Volume normalization
+tmp1 = trapz(p,squeeze(sum(srhoxi,3)),2); %Original radial profile
+tmp2=sum(beam_data.birth,1); %reference deposition profile
+tmp2=interp1(beam_data.dist_rhoaxis,tmp2,c,'linear','extrap')';
+if lnormalize
+    srhoxi=srhoxi.*tmp2./tmp1; %Apply normalization: reference/calculated profile
+    tmp4 = trapz(p,squeeze(sum(srhoxi,3)),2);
+end
 beam_data.srhoxi=srhoxi;
-beam_data.V=v;
-beam_data.c=c;
+beam_data.srhoxi_V=v;
+beam_data.srhoxi_Vc=c;
+beam_data.srhoxi_pitchedges=pitchedges;
+beam_data.srhoxi_pc=p;
+
 if lplot
-pixplot(c,pitchedges,squeeze(sum(srhoxi,3)))
-colorbar
-figure
-hold on
-plot(c,tmp1,'DisplayName','Integral from S(rho,xi)')
-plot(c,tmp4,'DisplayName','Renorm S(rho,xi)')
-plot(c,tmp2,'--','DisplayName','From calc depo')
-legend()
+    figure;
+    pixplot(c,p,squeeze(sum(srhoxi,3)))
+    colorbar
+    figure;
+    hold on
+    plot(c,tmp1,'DisplayName','Integral from S(rho,xi)')
+    if lnormalize
+        plot(c,tmp4,'DisplayName','Renorm S(rho,xi)')
+    end
+    plot(c,tmp2,'--','DisplayName','From calc depo')
+    legend()
 end
 end
